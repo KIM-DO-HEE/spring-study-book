b894d978f2692d626caedb78d0bc6dbfde2af62c
KIM-DO-HEE <kjkj014209@gmail.com>
2021-11-09 01:11:09 +0900

    머스테치로 글 수정, 삭제 화면 구성 - 글 수정, 삭제 기능 연동

--- src/main/java/com/example/springstudybook/service/posts/PostsService.java
+++ src/main/java/com/example/springstudybook/service/posts/PostsService.java
@@ -2,11 +2,15 @@ package com.example.springstudybook.service.posts;
 
 import com.example.springstudybook.domain.posts.Posts;
 import com.example.springstudybook.domain.posts.PostsRepository;
+import com.example.springstudybook.web.dto.PostsListResponseDto;
 import com.example.springstudybook.web.dto.PostsResponseDto;
 import com.example.springstudybook.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,19 @@ public class PostsService {
         Posts posts = postsRepository.findById(id).orElseThrow(()->
                 new IllegalArgumentException(("해당 게시글이 없습니다 id ="+ id)));
         return new PostsResponseDto(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void deleteById(Long id)
+    {
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시물이 없습니다 id ="+ id));
+        postsRepository.deleteById(posts.getId());
     }
 }
--- src/main/java/com/example/springstudybook/web/IndexController.java
+++ src/main/java/com/example/springstudybook/web/IndexController.java
@@ -1,15 +1,26 @@
 package com.example.springstudybook.web;
 
+import com.example.springstudybook.service.posts.PostsService;
+import com.example.springstudybook.web.dto.PostsListResponseDto;
+import com.example.springstudybook.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.dom4j.rule.Mode;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";  //앞의 경로는 src/main/template, 뒤의 경로는 mustache로 지정된다
 //       즉 여기서 src/main/templaate/index.mustache로 반환되어 ViewResolver가 처리하게된다
     }
@@ -19,5 +30,13 @@ public class IndexController {
         return "posts-save";
     }
 
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+
+        return "posts-update";
+
+    }
 
 }
